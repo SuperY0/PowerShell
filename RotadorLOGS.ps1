20f890594f973201b520f6a3ec99ce73b4781bb3
elgordodevops <samaniego715@hotmail.com>
2023-07-02 16:34:38 -0300

    Update RotadorLOGS.ps1

--- RotadorLOGS.ps1
+++ RotadorLOGS.ps1
@@ -1,51 +1,52 @@
-$sourceDirs = @(
-    "C:\Users\RS\OneDrive\Escritorio\Test1",
-    "C:\Users\RS\OneDrive\Escritorio\Test2"
-)
+# Author: https://github.com/elgordodevops
+# Script pensado para correrlo después de las 00:01, ya que busca el log del día anterior y nombra el archivo comprimido con el día anterior a la ejecución
 
-foreach ($dir in $sourceDirs) {
-    $backupDir = Join-Path $dir "BackupZip"
-    if (!(Test-Path $backupDir)) {
-        New-Item -ItemType Directory -Path $backupDir | Out-Null
+# Obtener fecha anterior a la ejecución del proceso
+$fechaAnterior = (Get-Date).AddDays(-1)
+
+# Obtener los posibles formatos de sufijos de fecha
+$sufijosFechaArchivo = @($fechaAnterior.ToString("yyyy-MM-dd"), $fechaAnterior.ToString("yyyyMMdd"))
+
+# Obtener la lista de directorios
+$listaDirectorios = @("C:\RotacionLOGS\Directorio1", "C:\RotacionLOGS\Directorio2", "C:\RotacionLOGS\Directorio3")
+
+# Recorrer la lista de directorios
+foreach ($directorio in $listaDirectorios) {
+    # Ruta de la subcarpeta de backup
+    $rutaBackup = Join-Path -Path $directorio -ChildPath "OldLogsZip"
+
+    # Verificar si existe la subcarpeta de backup y la crea si no existe
+    if (-not (Test-Path $rutaBackup)) {
+        New-Item -ItemType Directory -Path $rutaBackup | Out-Null
     }
-}
 
-for ($j = 0; $j -lt $sourceDirs.Count; $j++) {
-    $sourceDir = $sourceDirs[$j]
-    $destinationDir = Join-Path $sourceDir "BackupZip"
+    # Caso 1: Busca archivos con multiple nombre y formato
+    $prefijosNombresArchivo = @("ServiceLayer-", "ServiceLayer")
+    $extensionesArchivo = @(".log", ".txt")
 
-    #Caso1: todos los dias a las 00:00 se genera un archivo nuevo de log con fecha actual, entonces el el directorio a las 00:00
-    #existiran 2 archivos, ejemplo, uno llamado Archivo_20230313 y otro (Archivo_20230314 <--que se genero a las 00:00)
-    #Solucion: Buscar el archivo: ( prefijo "Archivo_" + fecha de jecucion del script 7 dias atras por si los findes no genera archivos
-    #Codigo: Busca el archivo $prefijo + la fecha anterior a la que se ejecuta el script(7 dias para atras), lo zipea y borra
-    $Prefijoarchivo = "ServiceLayer-"
-    for ($i = 1; $i -le 7; $i++) {
-        $fileDate = (Get-Date).AddDays(-$i).ToString("yyyy-MM-dd")
-        $fileName = $Prefijoarchivo + $fileDate + ".log"
-        if (Test-Path "$sourceDir\$fileName") {
-            $zipName = $fileName + ".zip" # Nombre del archivo ZIP
-            $zipFile = Join-Path $destinationDir $zipName # Ruta completa del archivo ZIP en la carpeta de destino
-            Compress-Archive -Path "$sourceDir\$fileName" -DestinationPath $zipFile -CompressionLevel Optimal
-            Remove-Item "$sourceDir\$fileName" # Elimina el archivo original después de comprimirlo
-            break # Detiene el ciclo for si se encuentra y comprime el archivo
+    foreach ($prefijo in $prefijosNombresArchivo) {
+        foreach ($sufijo in $sufijosFechaArchivo) {
+            foreach ($extension in $extensionesArchivo) {
+                $archivo = Join-Path -Path $directorio -ChildPath ("$prefijo$sufijo$extension")
+                if (Test-Path $archivo) {
+                    $nombreArchivoZip = Join-Path -Path $rutaBackup -ChildPath ("$prefijo$sufijo.zip")
+                    Compress-Archive -Path $archivo -DestinationPath $nombreArchivoZip -Force
+                    Remove-Item -Path $archivo -Force
+                    break
+                }
+            }
         }
     }
 
-    #Caso2: Archivo LOG que siempre tiene el mismo nombre pero incrementa el contenido
-    #Solucion: Busca el archivo de la variable $ArchivoCompleto, lo zipea con fecha anterior a la ejecucion y elimina (al la app que logea creara otro)      
-    # Busca el archivo a comprimir con nombre genérico
-    $NombreCompletoArchivo = "application.log"
-    if (Test-Path "$sourceDir\$NombreCompletoArchivo") {
-        $zipName = $NombreCompletoArchivo + "_" + (Get-Date).AddDays(-1).ToString("yyyy-MM-dd") + ".zip" # Nombre del archivo ZIP con la fecha actual menos un día
-        $zipFile = Join-Path $destinationDir $zipName # Ruta completa del archivo ZIP en la carpeta de destino
-        Compress-Archive -Path "$sourceDir\$NombreCompletoArchivo" -DestinationPath $zipFile -CompressionLevel Optimal
-        Remove-Item "$sourceDir\$NombreCompletoArchivo"
-    }
-}
+    # Caso 2: Busca archivos con nombres predefinidos
+    $archivosPredefinidos = @("application.log", "application1.log", "application2.log", "application3.txt", "application4.txt")
 
-# Elimina los archivos ZIP antiguos de los subdirectorios "Backup"
-$maxAge = 30 #días
-foreach ($dir in $sourceDirs) {
-    $backupDir = Join-Path $dir "Backup"
-    Get-ChildItem -Path $backupDir -Filter "*.zip" | Where-Object { $_.LastWriteTime -lt (Get-Date).AddDays(-$maxAge) } | Remove-Item -Force
+    foreach ($archivoPredefinido in $archivosPredefinidos) {
+        $rutaArchivoPredefinido = Join-Path -Path $directorio -ChildPath $archivoPredefinido
+        if (Test-Path $rutaArchivoPredefinido) {
+            $nombreArchivoZip = Join-Path -Path $rutaBackup -ChildPath ($archivoPredefinido + ".zip")
+            Compress-Archive -Path $rutaArchivoPredefinido -DestinationPath $nombreArchivoZip -Force
+            Remove-Item -Path $rutaArchivoPredefinido -Force
+        }
+    }
 }
